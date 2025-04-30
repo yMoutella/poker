5010a94e74f3835b2c58ba5af54169325fd6e150
Yure Moutella <ymoutella@gmail.com>
2025-04-30 17:02:25 -0300

    Added invitation link section to the user join a team with a invitation

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -2,7 +2,14 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
@@ -36,93 +43,114 @@ export default function MinimalPage() {
 
 
   return (
-    <div className="min-h-screen p-10">
+    <div className="min-h-screen p-10 ">
       <header className="mb-12">
-        <h1 className="text-3xl font-bold text-left">Teams</h1>
+        <h1 className="text-5xl font-bold text-left">Teams</h1>
       </header>
-
-      <main className="flex flex-col items-center">
-        <div className="w-full max-w-md space-y-1">
-          <Card className="rounded-2xl">
-            <CardContent className="p-3 space-y-4">
-              <div className="flex justify-end">
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button variant="outline">Create new team</Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                      <DialogTitle>New team</DialogTitle>
-                      <DialogDescription>
-                        Create your new team here. You'll be able to add mates in the team main page.
-                      </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                      <div className="items-center gap-4">
-                        <Input id="name" value="" placeholder="Team name" className="col-span-3" />
-                      </div>
-                    </div>
-                    <DialogFooter className="sm:justify-between">
-                      <DialogClose asChild>
-                        <Button type="button" variant="secondary">
-                          Cancel
-                        </Button>
-                      </DialogClose>
-                      <Button type="submit">Save changes</Button>
-                    </DialogFooter>
-                  </DialogContent>
-                </Dialog>
+      <div className="flex flex-col items-end mb-1.5">
+        <div className="flex justify-end ">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Create new team</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>New team</DialogTitle>
+                <DialogDescription>
+                  Create your new team here. You'll be able to add mates in the team main page.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="items-center gap-4">
+                  <Input id="name" value="" placeholder="Team name" className="col-span-3" />
+                </div>
               </div>
-              <ScrollArea className="h-64 space-y-2">
-                <TooltipProvider>
-                  {items.map((item, index) => {
-                    const isSelected = selected === item.name;
-                    return (
-                      <Tooltip key={index}>
-                        <TooltipTrigger asChild>
-                          <div
-                            onClick={() => setSelected(item.name)}
-                            className={`flex items-center justify-between p-4 border rounded-lg transition-all cursor-pointer hover:bg-accent ${isSelected ? "border-ring bg-muted" : ""
-                              } mb-2`}
-                          >
-                            <div className="flex items-center space-x-3">
-                              <Avatar>
-                                <AvatarFallback>
-                                  {item.name.charAt(0)}
-                                </AvatarFallback>
-                              </Avatar>
-                              <div>
-                                <p className="font-medium leading-none">
-                                  {item.name}
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                  Last active: {item.date}
-                                </p>
+              <DialogFooter className="sm:justify-between">
+                <DialogClose asChild>
+                  <Button type="button" variant="secondary">
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+      <div className="">
+        <main className="flex flex-col items-center">
+          <div className="w-full max-lg space-y-1">
+            <Card className="rounded-2xl">
+              <CardContent className="p-3 space-y-4">
+                <ScrollArea className="h-64 space-y-2">
+                  <TooltipProvider>
+                    {items.map((item, index) => {
+                      const isSelected = selected === item.name;
+                      return (
+                        <Tooltip key={index}>
+                          <TooltipTrigger asChild>
+                            <div
+                              onClick={() => setSelected(item.name)}
+                              className={`flex items-center justify-between p-4 border rounded-lg transition-all cursor-pointer hover:bg-accent ${isSelected ? "border-ring bg-muted" : ""
+                                } mb-2`}
+                            >
+                              <div className="flex items-center space-x-3">
+                                <Avatar>
+                                  <AvatarFallback>
+                                    {item.name.charAt(0)}
+                                  </AvatarFallback>
+                                </Avatar>
+                                <div>
+                                  <p className="font-medium leading-none">
+                                    {item.name}
+                                  </p>
+                                  <p className="text-sm text-muted-foreground">
+                                    Last active: {item.date}
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="flex items-center space-x-2">
+                                <Info className="w-4 h-4 text-muted-foreground" />
+
                               </div>
                             </div>
-                            <div className="flex items-center space-x-2">
-                              <Info className="w-4 h-4 text-muted-foreground" />
-
-                            </div>
-                          </div>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Created: {item.date}</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    );
-                  })}
-                </TooltipProvider>
-              </ScrollArea>
-              {selected && (
-                <Button className="flex align-middle justify-center self-center">Open: {selected}</Button>
-              )}
-            </CardContent>
-          </Card>
-        </div>
-      </main>
-
-
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Created: {item.date}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      );
+                    })}
+                  </TooltipProvider>
+                </ScrollArea>
+                {selected && (
+                  <Button className="flex align-middle justify-center self-center">Open: {selected}</Button>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+        </main>
+        <Card className="mt-3">
+          <CardHeader>
+            <CardTitle>I have a team link:</CardTitle>
+            <CardDescription>If you got a invitation link, insert below</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Input id="name" className="text-center" placeholder="Invitation link" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button className="w-1/6 h-10/12">Enter</Button>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
